cb5bfaa97d1aa220bcb7e8df412a5fc2930bf019
greenmooseSE <juggernaut78@gmail.com>
2020-04-24 08:55:28 +0200

    #1 Add repro solution [try-#1-ncNullRefDuringAnalyze-4.2]

---
+++ NUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NCTest
+{
+    [TestFixture(TypeArgs = new[] { typeof(string) })]
+    internal class NUnitTest<TOne>
+    {
+        [Test]
+        public void SomeTest()
+        {
+        }
+    }
+}
